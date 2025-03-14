cd6ad71d8c715581a970e6646bb4a7f9376f5109
Shamin Yasar <113526109+shamin2@users.noreply.github.com>
2025-03-14 14:33:26 -0230

    learnt something abouter router, more to wrok with later

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, UiService, Subscription, Router, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -14,7 +16,7 @@ export class HeaderComponent {
   showAddTask: boolean = true;
   subscription: Subscription;
 
-  constructor(private uiService: UiService){
+  constructor(private uiService: UiService, private router:Router){
     this.subscription = this.uiService.onToggle()
     .subscribe((value) => (this.showAddTask = value));
 
@@ -24,4 +26,7 @@ export class HeaderComponent {
     this.uiService.toggleAddTask();
   }
 
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
 }
